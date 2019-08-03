15e113966be4e05229d16c78ea0e159d6758088e
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-03 17:53:53 +0800

    wroking on navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,17 @@
 import React, { Fragment } from 'react';
+import { Menu } from 'semantic-ui-react';
 
 const Navbar = () => {
-	return <Fragment>navbar</Fragment>;
+	return (
+		<Menu color="red" inverted>
+			<Menu.Item name="editorials">Editorials</Menu.Item>
+
+			<Menu.Item name="reviews">Reviews</Menu.Item>
+
+			<Menu.Item name="upcomingEvents">Upcoming Events</Menu.Item>
+			<Menu.Item name="logout" position="right" />
+		</Menu>
+	);
 };
 
 export default Navbar;
