e099d122ff84ecd158863f8a0a404d263fa917cb
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-04 04:27:42 +0800

    Navbar complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
+import './styles/main.scss';
 
 const App = () => {
 	return (
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,29 @@
 import React, { Fragment } from 'react';
-import { Menu } from 'semantic-ui-react';
+import logo from '../../img/logo.jpg';
 
 const Navbar = () => {
 	return (
-		<Menu color="red" inverted>
-			<Menu.Item name="editorials">Editorials</Menu.Item>
-
-			<Menu.Item name="reviews">Reviews</Menu.Item>
-
-			<Menu.Item name="upcomingEvents">Upcoming Events</Menu.Item>
-			<Menu.Item name="logout" position="right" />
-		</Menu>
+		<Fragment>
+			<ul className="navbar">
+				<div className="navbar__left">
+					<li className="navbar__item">
+						<img className="navbar__logo" src={logo} alt="logo" />
+					</li>
+					<div className="navbar__header-container">
+						<li className="navbar__item navbar__header">
+							Southern River Toastmasters
+						</li>
+						<li className="navbar__item navbar__header-caption">
+							A President's Distinguished Club
+						</li>
+					</div>
+				</div>
+				<div className="navbar__right">
+					<li className="navbar__item">News</li>
+					<li className="navbar__item">Login</li>
+				</div>
+			</ul>
+		</Fragment>
 	);
 };
 
