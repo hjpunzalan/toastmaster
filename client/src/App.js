aa10e63788a2502d1c5eae2357c0f160c75bdf48
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-03 17:15:18 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+
+const App = () => {
+	return (
+		<Fragment>
+			<Navbar />
+		</Fragment>
+	);
+};
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+
+const Navbar = () => {
+	return <Fragment>navbar</Fragment>;
+};
+
+export default Navbar;
