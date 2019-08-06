e74178837d794c0110bb994c314cb5a6bb016916
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-06 20:23:56 +0800

    finished login component need to work on error handling

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,9 +3,9 @@ import { LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
 export const loginUser = (formData, history) => dispatch => {
 	const user = {
 		email: 'hj.punzalan@hotmail.com',
-		password: 'friend168'
+		password: 'test123'
 	};
-	if (formData === user) {
+	if (formData.email === user.email && formData.password === user.password) {
 		dispatch({
 			type: LOGIN_SUCCESS,
 			payload: formData
