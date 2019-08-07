014bf36b90395fd7e3d098f4ac71dbd21cc835ec
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-08 00:09:38 +0800

    finished designing footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import tagline from '../../img/tagline.png';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
 		<div className="Footer">
-			<div className="Footer__left">LEFT</div>
-			<div className="Footer__middle">
-				<img src={tagline} className="Footer__tagline" alt="tagline" />
+			<div className="Footer__left">
+				<p className="Footer__text">
+					Every Wednesday 6:30pm at Bletchely Park Primary School
+				</p>
 			</div>
-			<div className="Footer__right">RIGHT</div>
+			<div className="Footer__middle">
+				<a href="https://www.toastmasters.org/">
+					<img src={tagline} className="Footer__tagline" alt="tagline" />
+				</a>
+			</div>
+			<p className="Footer__credit">Website by Jonathan Punzalan</p>
+			<div className="Footer__right">
+				<a
+					className="Footer__link"
+					href="https://www.facebook.com/SRTOASTMASTERSCLUB/">
+					<FaFacebook className="Footer__logo Footer__logo-facebook" />
+				</a>
+				<a
+					className="Footer__link"
+					href="https://www.instagram.com/sr_toastmasters/">
+					<FaInstagram className="Footer__logo Footer__logo-instagram" />
+				</a>
+			</div>
 		</div>
 	);
 };
