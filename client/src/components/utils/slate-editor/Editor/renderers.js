a3c05049849864900b7631ff38f3914bcf2af8e2
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-09 22:07:40 +0800

    finished applying slateEditor

--- client/src/components/utils/slate-editor/Editor/renderers.js
+++ client/src/components/utils/slate-editor/Editor/renderers.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
 export const renderMark = (props, editor, next) => {
-	switch (props.mark.type) {
+	const { children, mark, attributes } = props;
+
+	switch (mark.type) {
 		case 'bold':
-			return <strong>{props.children}</strong>;
-		// Add our new mark renderers...
+			return <strong {...attributes}>{children}</strong>;
 		case 'code':
-			return <code>{props.children}</code>;
+			return <code {...attributes}>{children}</code>;
 		case 'italic':
-			return <em>{props.children}</em>;
-		case 'strikethrough':
-			return <del>{props.children}</del>;
-		case 'underline':
-			return <u>{props.children}</u>;
+			return <em {...attributes}>{children}</em>;
+		case 'underlined':
+			return <u {...attributes}>{children}</u>;
+		default:
+			return next();
+	}
+};
+
+export const renderBlock = (props, editor, next) => {
+	const { attributes, children, node } = props;
+
+	switch (node.type) {
+		case 'block-quote':
+			return (
+				<blockquote className="Editor__blockquote" {...attributes}>
+					{children}
+				</blockquote>
+			);
+		case 'bulleted-list':
+			return <ul {...attributes}>{children}</ul>;
+		case 'heading-one':
+			return <h1 {...attributes}>{children}</h1>;
+		case 'heading-two':
+			return <h2 {...attributes}>{children}</h2>;
+		case 'list-item':
+			return <li {...attributes}>{children}</li>;
+		case 'numbered-list':
+			return <ol {...attributes}>{children}</ol>;
 		default:
 			return next();
 	}
