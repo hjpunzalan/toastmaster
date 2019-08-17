c91da828801b980f63f4e834b7baeef2ece731f1
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-17 15:13:15 +0800

    added global error handler and appError class with stack trace

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const postsRouter = require('./routes/postsRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -17,5 +19,16 @@ app.use(express.json({ limit: '10kb' })); // package will parse 10kb into meanin
 // Middleware that applies to '/api/' request
 app.use('/api/users', userRouter);
 app.use('/api/posts', postsRouter);
+
+// Handle all unhandled routes
+app.all('*', (req, res, next) => {
+	const err = new AppError(
+		`Can't find ${req.originalUrl} on this server!`,
+		404
+	);
+	next(err); // whatever is passed into next is assumed to be an error
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ server/utils/appError.js
@@ -0,0 +1,14 @@
+class AppError extends Error {
+	constructor(message, statusCode) {
+		//Object created from this class when calling an instance
+		super(message); //From Error class
+
+		this.statusCode = statusCode || 500;
+		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+		this.isOperational = true; // To identify operational errors from programming errors, Only operational errors are handled by this class
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+module.exports = AppError;
