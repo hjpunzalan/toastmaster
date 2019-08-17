1b66e89a883ad45538bc9bd02f0ef27e663dd3a1
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-18 00:14:28 +0800

    completed handling validation errors and duplicate errors

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -4,6 +4,28 @@ const handleCastErrorDB = err => {
 	const message = `Invalid ${err.path}: ${err.value}.`; //Invalid _id : wwwwwwww
 	return new AppError(message, 400);
 };
+const handleDuplicateFieldsDB = err => {
+	// regular expressions is always between two slashes '/'
+	const value = err.errmsg.match(/(["'])(\\?.)*?\1/); //reg expression between quotation marks
+	const message = `Duplicate field value: ${
+		//value returns an array
+		value[0]
+	}. Please use another value!`;
+	return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+	// Object.values converts object property values into an array with the object as argument
+	// err.errors return an object of errors with properties where theres validation errors
+	const errors = Object.values(err.errors)
+		.map(el => el.message)
+		.join('. ');
+	console.log(errors);
+
+	// Join array into a single string
+	const message = `Invalid input data. ${errors}`;
+	return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
@@ -16,6 +38,7 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
 	// Operational, trusted error: send message to client
+	// Production mode: Only send meaningful,concise and easy to understand errors
 	if (err.isOperational) {
 		res.status(err.statusCode).json({
 			status: err.status,
@@ -42,6 +65,9 @@ module.exports = (err, req, res, next) => {
 	} else if (process.env.NODE_ENV === 'production') {
 		let newError = { ...err };
 		if (err.name === 'CastError') newError = handleCastErrorDB(newError);
+		if (err.code === 11000) newError = handleDuplicateFieldsDB(newError);
+		if (err.name === 'ValidationError')
+			newError = handleValidationErrorDB(newError);
 		sendErrorProd(newError, res);
 	}
 
