ad102419031cbafe6d7e97ebf1984191bb1ca6de
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-08-18 11:58:28 +0800

    added handling unhandled rejections and exceptions async and sync in serverjs

--- server/server.js
+++ server/server.js
@@ -3,9 +3,18 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// SYNC
+// listening to event uncaughtException
+process.on('uncaughtException', err => {
+	console.log('UNCAUGHT Exception! Shutting down...');
+	console.log(err.name, err.message);
+	process.exit(1); // 0 success , 1 for unhanled rejection
+});
+//////////////
+
 dotenv.config({ path: './config.env' });
 
-// Conencting to mongoDB using mongoose
+// Connecting to mongoDB using mongoose
 const DB = process.env.DATABASE.replace(
 	'<PASSWORD>',
 	process.env.DATABASE_PASSWORD
@@ -21,6 +30,20 @@ mongoose
 console.log(`Server running on: ${process.env.NODE_ENV} mode`);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running on port ${port}`);
 });
+
+// ASYNC Promises
+// process object will emmitt unhandled rejection
+// promise rejection to have last safety nets
+process.on('unhandledRejection', err => {
+	console.log('UNHANDLED REJECTION! Shutting down...');
+	console.log(err.name, err.message);
+	server.close(() => {
+		//  BY having server.close finishes all request that is being handled then closes the app
+		process.exit(1); // 0 success , 1 for unhanled rejection
+	});
+});
+
+// Need a tool that restarts application
