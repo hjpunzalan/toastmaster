1ef093081de64b507dd1e832d3dd5612de90ee6c
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-09-02 11:42:17 +0800

    added upload photo on dashboard and aws s3 access keys

--- client/src/components/pages/Dashboard/Dashboard.js
+++ client/src/components/pages/Dashboard/Dashboard.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import img from '../../../img/anonymous.png';
 
-const Dashboard = () => {
+const Dashboard = ({ auth: { currentUser } }) => {
+	const [file, setFile] = useState(null);
+
+	const handleFileChange = e => {
+		setFile(e.target.files[0]);
+		console.log(e.target.files);
+	};
 	return (
 		<div className="Dashboard">
-			<h1 className="Dashboard__title">Member's Dashboard</h1>
-			<p className="Dashboard__label">
-				Here you will find club guidelines and resources available to its
-				members.
-			</p>
-			<hr />
+			<div className="Dashboard__left">
+				<h1 className="Dashboard__title">Member's Dashboard</h1>
+				<p className="Dashboard__label">
+					Here you will find club guidelines and resources available to its
+					members.
+				</p>
+				<hr />
+			</div>
+			<div className="Dashboard__right">
+				{currentUser && currentUser.img ? (
+					<img src={currentUser.img} alt="user avatar" />
+				) : (
+					<img src={img} alt="user avatar" />
+				)}
+				<div className="Dashboard__upload">
+					<h5>Add an Image</h5>
+					<input type="file" accept="image/*" onChange={handleFileChange} />
+				</div>
+			</div>
 		</div>
 	);
 };
+Dashboard.propTypes = {
+	auth: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+export default connect(mapStateToProps)(Dashboard);
