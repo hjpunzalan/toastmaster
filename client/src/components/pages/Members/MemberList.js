8a4861f693bb455749a776dbdaf5217a95b45ad7
Jonathan <hj.punzalan@hotmail.com>
2019-09-04 22:06:53 +0800

    started adding member list

--- client/src/components/pages/Members/MemberList.js
+++ client/src/components/pages/Members/MemberList.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getAllUsers } from '../../../actions/users';
+import img from '../../../img/anonymous.png';
 
-const MemberList = () => {
-	return <div></div>;
+const MemberList = ({ getAllUsers, users: { users } }) => {
+	useEffect(() => {
+		getAllUsers();
+		//eslint-disable-next-line
+	}, []);
+	return (
+		<div className='MemberList'>
+			{users.map(user => (
+				<div className='MemberList__member'>
+					<img src={user.photo || img} alt='user avatar' />
+				</div>
+			))}
+		</div>
+	);
 };
 
-export default MemberList;
+MemberList.propTypes = {
+	getAllUsers: PropTypes.func.isRequired,
+	users: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	users: state.users
+});
+
+export default connect(
+	mapStateToProps,
+	{ getAllUsers }
+)(MemberList);
