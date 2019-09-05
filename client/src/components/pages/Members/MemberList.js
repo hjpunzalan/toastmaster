9b22793dfb9b6f7000ed9f3fa53d0e5ed5d4cdbb
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-09-05 20:57:11 +0800

    finished memberList with sorting of name by firstName

--- client/src/components/pages/Members/MemberList.js
+++ client/src/components/pages/Members/MemberList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getAllUsers } from '../../../actions/users';
 import img from '../../../img/anonymous.png';
+import { MdEmail } from 'react-icons/md';
 
 const MemberList = ({ getAllUsers, users: { users } }) => {
 	useEffect(() => {
@@ -10,10 +11,28 @@ const MemberList = ({ getAllUsers, users: { users } }) => {
 		//eslint-disable-next-line
 	}, []);
 	return (
-		<div className='MemberList'>
+		<div className="MemberList">
+			<h1 className="MemberList__title">Member List</h1>
+			<hr />
 			{users.map(user => (
-				<div className='MemberList__member'>
-					<img src={user.photo || img} alt='user avatar' />
+				<div className="MemberList__member">
+					<img
+						className="MemberList__member-photo"
+						src={user.photo || img}
+						alt="user avatar"
+					/>
+					<span className="MemberList__member-name">
+						{user.firstName} {user.lastName}
+					</span>
+					<span style={{ display: 'flex', alignItems: 'center' }}>
+						<a
+							href={`mailto:${user.email}`}
+							target="_top"
+							className="MemberList__member-email">
+							<MdEmail />
+						</a>
+						{user.email}
+					</span>
 				</div>
 			))}
 		</div>
