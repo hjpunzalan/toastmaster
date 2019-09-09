e604595571a082a111e7d5098bc0e1348d5d245c
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-09-10 00:06:40 +0800

    Made announcement size more responsive

--- client/src/components/pages/Dashboard/Announcements.js
+++ client/src/components/pages/Dashboard/Announcements.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Announcements = () => {
 	let width = 750;
 	let height = 588;
-	if (window.screen.width < 800) {
+	if (window.screen.width < 1150) {
 		width = 500;
 		height = 441;
 	}
-	if (window.screen.width < 550) {
+	if (window.screen.width < 720) {
 		width = 400;
 		height = 398;
 	}
@@ -18,57 +18,33 @@ const Announcements = () => {
 
 	return (
 		<div className="Dashboard__left">
-			<h1 className="Dashboard__title">Announcements</h1>
-			<iframe
-				className="Dashboard__facebook"
-				title="Southern River Toastmaster Facebook"
-				src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
-				width={width}
-				height={height}
-				scrolling="no"
-				frameBorder="0"
-				allowtransparency="true"
-				allow="encrypted-media"></iframe>
-			<iframe
-				className="Dashboard__facebook"
-				title="Southern River Toastmaster Facebook"
-				src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
-				width={width}
-				height={height}
-				scrolling="no"
-				frameBorder="0"
-				allowtransparency="true"
-				allow="encrypted-media"></iframe>
-			<iframe
-				className="Dashboard__facebook"
-				title="Southern River Toastmaster Facebook"
-				src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
-				width={width}
-				height={height}
-				scrolling="no"
-				frameBorder="0"
-				allowtransparency="true"
-				allow="encrypted-media"></iframe>
-			<iframe
-				className="Dashboard__facebook"
-				title="Southern River Toastmaster Facebook"
-				src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
-				width={width}
-				height={height}
-				scrolling="no"
-				frameBorder="0"
-				allowtransparency="true"
-				allow="encrypted-media"></iframe>
-			<iframe
-				className="Dashboard__facebook"
-				title="Southern River Toastmaster Facebook"
-				src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
-				width={width}
-				height={height}
-				scrolling="no"
-				frameBorder="0"
-				allowtransparency="true"
-				allow="encrypted-media"></iframe>
+			<div className="Dashboard__title">
+				Announcements
+				<button className="btn btn__submit">Post new announcement</button>
+			</div>
+			<div className="Dashboard__announcements">
+				<iframe
+					className="Dashboard__facebook"
+					title="Southern River Toastmaster Facebook"
+					src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
+					width={width}
+					height={height}
+					scrolling="no"
+					frameBorder="0"
+					allowtransparency="true"
+					allow="encrypted-media"></iframe>
+
+				<iframe
+					className="Dashboard__facebook"
+					title="Southern River Toastmaster Facebook"
+					src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSRTOASTMASTERSCLUB%2Fposts%2F1714806421985710&width=500&show_text=true&appId=2411264755855685&height=${height}`}
+					width={width}
+					height={height}
+					scrolling="no"
+					frameBorder="0"
+					allowtransparency="true"
+					allow="encrypted-media"></iframe>
+			</div>
 		</div>
 	);
 };
