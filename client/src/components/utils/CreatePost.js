44c58765b95980c22326a3f06a22787b72f8c893
hjpunzalan <hj.punzalan@hotmail.com>
2019-09-19 14:44:55 +0800

    started to make creating post mobile responsive

--- client/src/components/utils/CreatePost.js
+++ client/src/components/utils/CreatePost.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextEditor from '../utils/draft-js/TextEditor';
 
 const CreatePost = ({
@@ -10,6 +10,10 @@ const CreatePost = ({
 	contentState,
 	withPlainText
 }) => {
+	// Need a state for text field
+	// Need to convert to contentState when editing
+	// If editing from mobile, need to use plain text only
+	// Need all post to have plain Text
 	return (
 		<div className="CreatePost">
 			<button className="btn btn__cancel" onClick={handleToggle}>
@@ -31,11 +35,23 @@ const CreatePost = ({
 					onChange={e => setTitle(e.target.value)}
 					maxLength={80} // so it doesnt pollute the post too much
 				/>
-				<TextEditor
-					withPlainText={withPlainText}
-					contentState={contentState}
-					handleSubmit={handleSubmit}
-				/>
+				{window.screen.width < 1000 ? (
+					<>
+						<input
+							className="CreatePost__editor"
+							type="text"
+							name="editor"
+							id="editor"
+						/>
+						<button className="btn btn__submit">Submit Post</button>
+					</>
+				) : (
+					<TextEditor
+						withPlainText={withPlainText} //true or false
+						contentState={contentState} // context
+						handleSubmit={handleSubmit} // submit function
+					/>
+				)}
 			</div>
 		</div>
 	);
