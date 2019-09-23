937afdfd4c63f02ffc75a2981aab51ba091c3fb8
hjpunzalan <hj.punzalan@hotmail.com>
2019-09-23 19:57:19 +0800

    to be deployed

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ app.use(globalErrorHandler);
 //  Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('../client/build'));
+	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 	});
 }
 module.exports = app;
