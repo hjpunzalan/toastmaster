89f572286101e5f2ac1b1cb1f6f3257a3e031f82
hjpunzalan <hj.punzalan@hotmail.com>
2019-09-28 13:05:25 +0800

    added a ping to keep app awake for 24 hrs

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const mongoose = require('mongoose');
 
+// ping site
+setInterval(function() {
+	app.get('http://srtoastmasters.herokuapp.com');
+}, 300000); // every 5 minutes (300000)
+
 // SYNC
 // listening to event uncaughtException
 process.on('uncaughtException', err => {
