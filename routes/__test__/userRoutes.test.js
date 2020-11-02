b0ad6f92eb1289d3f09e55d50e97b11edb7f9130
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-03 02:48:54 +0800

    feat: test get all users

--- routes/__test__/userRoutes.test.js
+++ routes/__test__/userRoutes.test.js
@@ -4,23 +4,58 @@ const Users = require("../../models/Users");
 const signAdmin = require("../../test/setup").signAdmin;
 const Email = require("../../utils/email");
 
-test("should create new account and send email", async () => {
-	const cookie = await signAdmin();
-	const email = "user@test.com";
+describe("With admin rights", () => {
+	test("should create new account and send email as admin", async () => {
+		const cookie = await signAdmin();
+		const email = "user@test.com";
 
-	await request(app)
-		.post("/api/users/register")
-		.set("Cookie", cookie)
-		.send({
-			firstName: "user",
-			lastName: "test",
-			email,
-		})
-		.expect(201);
+		await request(app)
+			.post("/api/users/register")
+			.set("Cookie", cookie)
+			.send({
+				firstName: "user",
+				lastName: "test",
+				email,
+			})
+			.expect(201);
 
-	const user = await Users.findOne({ email });
+		const user = await Users.findOne({ email });
 
-	expect(user.email).toEqual(email);
-	expect(Email).toHaveBeenCalledTimes(1);
-	expect(Email.mock.instances[0].sendWelcome).toHaveBeenCalledTimes(1);
+		expect(user.email).toEqual(email);
+		expect(Email).toHaveBeenCalledTimes(1);
+		expect(Email.mock.instances[0].sendWelcome).toHaveBeenCalledTimes(1);
+	});
+
+	test("Fail when registering user not as admin", async () => {
+		await request(app)
+			.post("/api/users/register")
+			.send({
+				firstName: "user",
+				lastName: "test",
+				email: "test@test.com",
+			})
+			.expect(401);
+	});
+
+	test("Get all users", async () => {
+		const cookie = await signAdmin();
+		const email = "user@test.com";
+
+		await request(app)
+			.post("/api/users/register")
+			.set("Cookie", cookie)
+			.send({
+				firstName: "user",
+				lastName: "test",
+				email,
+			})
+			.expect(201);
+		const response = await request(app)
+			.get("/api/users/")
+			.set("Cookie", cookie)
+			.send({})
+			.expect(200);
+
+		expect(response.body.length).toEqual(2);
+	});
 });
