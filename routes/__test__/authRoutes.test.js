040d61882f1925ca5e9ba9021eabb01ef20ccccf
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-03 12:58:46 +0800

    feat: test user is logged in, logout, forgotpassword, resetpassword

---
+++ routes/__test__/authRoutes.test.js
@@ -0,0 +1,68 @@
+const request = require("supertest");
+const app = require("../../app");
+const Users = require("../../models/Users");
+const signAdmin = require("../../test/setup").signAdmin;
+const Email = require("../../utils/email");
+
+test("should check user is logged in", async () => {
+	const cookie = await signAdmin();
+
+	await request(app)
+		.get("/api/auth/checkUser")
+		.set("Cookie", cookie)
+		.send()
+		.expect(200);
+});
+
+test("should logout user", async () => {
+	const cookie = await signAdmin();
+
+	await request(app)
+		.get("/api/auth/logout")
+		.set("Cookie", cookie)
+		.send()
+		.expect(200);
+});
+
+test("should able to request forgot password, send reset token to email, and reset password", async () => {
+	const cookie = await signAdmin();
+	// Create a user
+	const {
+		body: { email },
+	} = await request(app)
+		.post("/api/users/register")
+		.set("Cookie", cookie)
+		.send({
+			firstName: "user",
+			lastName: "test",
+			email: "user@test.com",
+		})
+		.expect(201);
+
+	// Request password reset
+	await request(app)
+		.post("/api/auth/forgotPassword")
+		.send({ email })
+		.expect(200);
+
+	// Should generate a hashed password reset token and expires less than 10 minutes
+	const user = await Users.findOne({ email });
+	expect(user.passwordResetToken).toBeDefined();
+	expect(user.passwordResetExpires - Date.now()).toBeLessThanOrEqual(
+		10 * 60 * 1000
+	);
+
+	// Should send a reset password email
+	expect(Email).toHaveBeenCalledTimes(2); //one for registering user
+	expect(Email.mock.instances[0].sendWelcome).toHaveBeenCalledTimes(1);
+	expect(Email.mock.instances[1].sendPasswordReset).toHaveBeenCalledTimes(1);
+
+	// Request password reset
+	const resetURL = Email.mock.instances[1].constructor.mock.calls[1][1];
+	const resetToken = resetURL.replace("undefined/reset/", "");
+
+	await request(app)
+		.patch(`/api/auth/resetPassword/${resetToken}`)
+		.send({ password: "test123" })
+		.expect(200);
+});
