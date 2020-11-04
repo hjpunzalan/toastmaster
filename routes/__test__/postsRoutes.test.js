cacbdae1a0b22122874232d85932358cf1404733
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-04 23:49:51 +0800

    should be able to search post by title or plainText with pagination query

--- routes/__test__/postsRoutes.test.js
+++ routes/__test__/postsRoutes.test.js
@@ -30,7 +30,7 @@ test("should create and get all posts with page and limit query", async () => {
 
 	// Send query for 1 post limit only
 	const {
-		body: { results },
+		body: { results, numPosts },
 	} = await request(app)
 		.get(`/api/posts?page=1&limit=1&sort=-lastComment,-date`)
 		.set("Cookie", cookie)
@@ -38,4 +38,44 @@ test("should create and get all posts with page and limit query", async () => {
 		.expect(200);
 
 	expect(results).toEqual(1);
+	expect(numPosts).toEqual(2);
+});
+
+test("should be able to search post by title or plainText with pagination query", async () => {
+	const { cookie } = await signUser("user");
+
+	// Create two posts
+	await request(app)
+		.post("/api/posts/")
+		.set("Cookie", cookie)
+		.send({
+			title: "candy",
+			plainText: "test",
+			contentState: {},
+		})
+		.expect(201);
+
+	await request(app)
+		.post("/api/posts/")
+		.set("Cookie", cookie)
+		.send({
+			title: "test2",
+			plainText: "candy",
+			contentState: {},
+		})
+		.expect(201);
+
+	// Send query for 1 post limit only
+	const {
+		body: { posts, numPosts },
+	} = await request(app)
+		.post(`/api/posts/search/text?page=1&limit=1&sort=-lastComment,-date`)
+		.set("Cookie", cookie)
+		.send({
+			text: "test",
+		})
+		.expect(200);
+
+	expect(posts.length).toEqual(1);
+	expect(numPosts).toEqual(2);
 });
