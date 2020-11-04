722b89ad928c23d01887d131c185bdb8b85f05b0
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-04 23:42:37 +0800

    should create and get all posts with page and limit query

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,15 +1,15 @@
-const Posts = require('../models/Posts');
-const catchAsync = require('../utils/catchAsync');
-const QueryHandling = require('../utils/queryHandling');
-const AppError = require('../utils/appError');
-const checkBody = require('../utils/checkBody');
+const Posts = require("../models/Posts");
+const catchAsync = require("../utils/catchAsync");
+const QueryHandling = require("../utils/queryHandling");
+const AppError = require("../utils/appError");
+const checkBody = require("../utils/checkBody");
 
 exports.createPost = catchAsync(async (req, res, next) => {
 	const newPost = new Posts({
 		user: req.user.id,
 		title: req.body.title,
 		contentState: req.body.contentState,
-		plainText: req.body.plainText
+		plainText: req.body.plainText,
 	});
 
 	await newPost.save();
@@ -25,7 +25,7 @@ exports.getAllPosts = catchAsync(async (req, res, next) => {
 	res.status(200).json({
 		numPosts,
 		results: allPosts.length,
-		posts: allPosts
+		posts: allPosts,
 	});
 });
 
@@ -33,22 +33,22 @@ exports.getPost = catchAsync(async (req, res, next) => {
 	const query = Posts.findById(req.params.id);
 	const features = new QueryHandling(query, req.query);
 	const post = await features.query;
-	if (!post) return next(new AppError('No post was found with this Id.', 404));
+	if (!post) return next(new AppError("No post was found with this Id.", 404));
 	res.status(200).json(post);
 });
 
 exports.editPost = catchAsync(async (req, res, next) => {
 	const filterBody = checkBody(req.body, next, [
-		'title',
-		'contentState',
-		'plainText'
+		"title",
+		"contentState",
+		"plainText",
 	]);
 	const post = await Posts.findByIdAndUpdate(req.params.id, filterBody, {
 		new: true,
-		runValidators: true
+		runValidators: true,
 	});
 	if (!post) {
-		return next(new AppError('No post was found with this Id.', 404));
+		return next(new AppError("No post was found with this Id.", 404));
 	}
 	res.status(200).json(post);
 });
@@ -56,22 +56,22 @@ exports.editPost = catchAsync(async (req, res, next) => {
 exports.deletePost = catchAsync(async (req, res, next) => {
 	const post = await Posts.findByIdAndRemove(req.params.id);
 	if (!post) {
-		return next(new AppError('No post was found with this Id.', 404));
+		return next(new AppError("No post was found with this Id.", 404));
 	}
 
 	res.status(204).json({
-		status: 'success'
+		status: "success",
 	});
 });
 
 exports.addComment = catchAsync(async (req, res, next) => {
 	const post = await Posts.findById(req.params.id);
 	if (!post) {
-		return next(new AppError('No post was found with this Id.', 404));
+		return next(new AppError("No post was found with this Id.", 404));
 	}
 	const newComment = {
 		user: req.user.id,
-		contentState: req.body.contentState
+		contentState: req.body.contentState,
 	};
 
 	post.comments.push(newComment);
@@ -85,11 +85,13 @@ exports.addComment = catchAsync(async (req, res, next) => {
 exports.deleteComment = catchAsync(async (req, res, next) => {
 	const post = await Posts.findById(req.params.postId);
 	if (!post) {
-		return next(new AppError('No post was found with this Id.', 404));
+		return next(new AppError("No post was found with this Id.", 404));
 	}
-	const newComments = post.comments.filter(c => c._id != req.params.commentId); //req.params is a number
+	const newComments = post.comments.filter(
+		(c) => c._id != req.params.commentId
+	); //req.params is a number
 	if (post.comments.length === newComments.length) {
-		return next(new AppError('Comment not found.', 404));
+		return next(new AppError("Comment not found.", 404));
 	}
 	post.comments = newComments;
 	// after filtered
@@ -104,19 +106,19 @@ exports.searchPost = catchAsync(async (req, res, next) => {
 	// Using index limits the need of searching the whole collection
 	const search = {
 		$or: [
-			{ plainText: { $regex: req.body.text, $options: 'i' } }, //included insensitive casing options
-			{ title: { $regex: req.body.text, $options: 'i' } }
-		]
+			{ plainText: { $regex: req.body.text, $options: "i" } }, //included insensitive casing options
+			{ title: { $regex: req.body.text, $options: "i" } },
+		],
 	};
 	const query = Posts.find(search);
 	const features = new QueryHandling(query, req.query).sort().paginate();
 	const posts = await features.query;
 	const numPosts = await Posts.find(search).countDocuments();
 	if (posts.length === 0) {
-		return next(new AppError('No results found', 400));
+		return next(new AppError("No results found", 400));
 	}
 	res.status(200).json({
 		posts,
-		numPosts
+		numPosts,
 	});
 });
