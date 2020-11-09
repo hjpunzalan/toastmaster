c70c7f2b8ee6b8c89316817bf5b049ed871d4382
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-09 22:31:03 +0800

    setup upload route test

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,23 +1,24 @@
-const AWS = require('aws-sdk');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const AWS = require("aws-sdk");
+const catchAsync = require("../utils/catchAsync");
 
 const connectToS3 = () => {
 	return new AWS.S3({
 		accessKeyId: process.env.S3_ACCESS_KEY,
-		secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+		secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 	});
 };
 
 exports.uploadToS3 = catchAsync(async (req, res, next) => {
+	// Todo fix s3 upload with version for each upload deleting previous version
+	// This fixes identical photo addresses with persistent cached image
 	const s3 = connectToS3();
 	const key = `${req.user.id}/photo.jpeg`;
 	const params = {
-		Bucket: 'toastmaster-user-photo',
-		ContentType: 'image/jpeg',
-		Key: key
+		Bucket: "toastmaster-user-photo",
+		ContentType: "image/jpeg",
+		Key: key,
 	};
-	await s3.getSignedUrl('putObject', params, (err, url) => {
+	await s3.getSignedUrl("putObject", params, (err, url) => {
 		res.status(200).json({ key, url });
 	});
 });
---
+++ routes/__test__/uploadRoutes.test.js
@@ -0,0 +1,5 @@
+const request = require("supertest");
+const app = require("../../app");
+const signUser = require("../../test/setup").signUser;
+
+test("should upload new photo version to AWS and update user photo version", () => {});
