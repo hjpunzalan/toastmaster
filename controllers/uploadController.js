63c82683e94aaaa3001c17c3417a116e74d28c3b
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-12 10:07:09 +0800

    Brought back signedURL with custom versioning to upload photos

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -9,37 +9,47 @@ const connectToS3 = () => {
 };
 
 exports.uploadToS3 = catchAsync(async (req, res, next) => {
-	console.log(req.body);
 	// Todo fix s3 upload with version for each upload deleting previous version
 	// This fixes identical photo addresses with persistent cached image
-	// update the key to include version
+	// Update the key to include version with custom versioning
+	// SignedURL gives no response, must use custom versioning!
 	// Delete previous version
-	// REMOVE PRE SIGNED URL
+	const imageType = req.body.type.replace("image/", "");
+
+
+	// Establish old version
+	// Delete null versions if previously set already
+	let oldVersion = null;
+		if (req.user.photo.includes("-versionId=")) {
+		oldVersion = req.user.photo.split(`-versionId=`)[1];
+	}
+	
+
+	// Set params
 	const s3 = connectToS3();
-	const key = `${req.user.id}/photo.${req.body.type.replace("image/", "")}`;
-	let params = {
+	const key = `${req.user.id}/photo-versionId=${oldVersion+1}.${imageType}`;
+	const params = {
 		Bucket: "toastmaster-user-photo",
 		ContentType: req.body.type,
 		Key: key,
 	};
 
-	// Put photo into bucket as an object
-	await s3.putObject(params, async (err, data) => {
+	// Provide url to upload object to s3 bucket
+	// Prevents sending large image file to server and instead,
+	//is sent directly to AWS
+	await s3.getSignedUrl("putObject", params, (err, url) => {
 		// Delete previous version
-		if (req.user.photo.includes("versiondId")) {
-			// Extract old versionId
-			const oldVersion = req.user.photo.split("versionId=")[1];
+		// WARNING: Must be called together with signedURL from client!!
+		
+		if(req.user.photo){
+		await s3.deleteObject({ ...params, VersionId: oldVersion }, (err, data) => {
+			if (err) {
+				console.error(err);
+					return next(new AppError("Something went wrong...", 500));
+			}
+		})}
 
-			params = {
-				...params,
-				VersionId: oldVersion,
-			};
-		}
-		await s3.deleteObject(params, err);
-
-		// Send new photo link
-		res.status(200).json({
-			key: `${key}?versionId=${data.VersionId}`,
-		});
+		// Send url
+		res.status(200).json({ key, url });
 	});
 });
