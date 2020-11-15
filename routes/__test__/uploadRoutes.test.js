183cc4c6e71177ee823fe7362c6bddac895c2172
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-15 22:39:27 +0800

    test: request to upload photo - requires mock function

--- routes/__test__/uploadRoutes.test.js
+++ routes/__test__/uploadRoutes.test.js
@@ -2,4 +2,12 @@ const request = require("supertest");
 const app = require("../../app");
 const signUser = require("../../test/setup").signUser;
 
-test("should upload new photo version to AWS and update user photo version", () => {});
+test("request to upload photo should return s3 bucket key and signed url", async () => {
+	const { cookie } = await signUser("user");
+
+	await request(app)
+		.post("/api/upload/")
+		.set("Cookie", cookie)
+		.send({ type: "image/jpeg" })
+		.expect(200);
+});
