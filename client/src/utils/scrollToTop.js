c509e9a207c653a711f548fbf4e715e9b30c7d32
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-18 22:41:36 +0800

    fix: scrollToTop removed animation

--- client/src/utils/scrollToTop.js
+++ client/src/utils/scrollToTop.js
@@ -1,9 +1,6 @@
 const scrollToTop = () => {
-	const c = document.documentElement.scrollTop || document.body.scrollTop;
-	if (c > 0) {
-		window.requestAnimationFrame(scrollToTop);
-		window.scrollTo(0, c - c / 8);
-	}
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 };
 
 export default scrollToTop;
