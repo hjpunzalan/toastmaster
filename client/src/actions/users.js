08ae7b0fbcb22f4b9735c53f759a6b86dbc3e7c8
hjpunzalan <hj.punzalan@hotmail.com>
2020-11-29 21:00:09 +0800

    fix: user action comments added and remove unneccesities

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -16,18 +16,16 @@ import catchAsync from "../utils/catchAsync";
 
 export const registerUser = (formData, url) =>
 	catchAsync("user", async (dispatch) => {
+		// Reset alert if there is register user error alert
 		dispatch(resetAlert());
+		// Deploy loading screen
 		dispatch({ type: LOADING_USER });
-		const res = await axios.post(
-			"/api/users/register",
-			{ ...formData, url },
-			{
-				headers: {
-					"Content-type": "application/json",
-				},
-			}
-		);
+		// send form data and current page url to send link with jwt token to user email
+		const res = await axios.post("/api/users/register", { ...formData, url });
+		// dispatch register action
 		dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+
+		// send alert to user
 		dispatch(
 			setAlert(
 				`${formData.firstName} ${formData.lastName} was successfully registered and temporary password sent to user's email`,
@@ -38,39 +36,45 @@ export const registerUser = (formData, url) =>
 
 export const updateMe = (formData, file, history) =>
 	catchAsync("update", async (dispatch) => {
+		// Reset alert if there was error
 		dispatch(resetAlert());
+		// Deploy loading screen
 		dispatch({ type: LOADING_AUTH });
-
 		if (file) {
-			// Send file
+			// Send request for aws s3 url config
 			const uploadConfig = await axios.post("/api/upload", { type: file.type });
-
+			// Upload photo to AWS S3 bucket
 			await axios.put(uploadConfig.data.url, file, {
 				headers: {
 					"Content-Type": file.type,
 				},
 			});
-			// Set photo link
+			// Set photo link to update user details
 			formData.photo = `https://toastmaster-user-photo.s3-ap-southeast-2.amazonaws.com/${uploadConfig.data.key}`;
 		}
+		// Send updated user details
 		const res = await axios.patch("/api/users/updateMe", formData, {
 			headers: {
 				"Content-Type": "application/json",
 			},
 		});
-
-		history.push("/dashboard");
+		// Dispatch updated user details
 		dispatch({
 			type: UPDATE_ME,
 			payload: res.data,
 		});
+		// Navigate user  back to dashboard
+		history.push("/dashboard");
+		// Send alert to user that details as updated
 		dispatch(setAlert("User updated", "success"));
 	});
 
 export const getAllUsers = () =>
 	catchAsync(async (dispatch) => {
+		// Send request to get all user sorted by first name
 		const res = await axios.get(`/api/users?sort=firstName`);
 
+		// Dispatch results to redux state
 		dispatch({
 			type: GET_ALL_USERS,
 			payload: res.data,
@@ -78,15 +82,20 @@ export const getAllUsers = () =>
 	});
 
 export const toggleView = () => (dispatch) =>
+	// Change view state if user is committee or admin
 	dispatch({ type: TOGGLE_MODERATOR });
 
 export const deActivateUser = (userId) =>
 	catchAsync("user", async (dispatch) => {
+		// Remove any previous alert or alert from error
 		dispatch(resetAlert());
+		// Dispatch loading page
 		dispatch({ type: LOADING_USER });
+		// Send request to deactivate user
 		const res = await axios.patch(`/api/users/deActivateUser/${userId}`);
-
+		// Dispatch deactivate user action
 		dispatch({ type: DEACTIVATE_USER, payload: res.data });
+		// Dispatch user deactivation to current user
 		dispatch(
 			setAlert(
 				`${res.data.firstName} ${res.data.lastName} has been deactivated!`,
@@ -97,11 +106,15 @@ export const deActivateUser = (userId) =>
 
 export const activateUser = (userId) =>
 	catchAsync("user", async (dispatch) => {
+		// Reset any previous alerts possibly from error alert
 		dispatch(resetAlert());
+		// Dispatch loading page
 		dispatch({ type: LOADING_USER });
+		// Make request to reactivate user
 		const res = await axios.patch(`/api/users/activateUser/${userId}`);
-
+		// Dispatch new user details to user list
 		dispatch({ type: ACTIVATE_USER, payload: res.data });
+		// Dispatch success alert to user
 		dispatch(
 			setAlert(
 				`${res.data.firstName} ${res.data.lastName} has been activated!`,
@@ -112,11 +125,16 @@ export const activateUser = (userId) =>
 
 export const changeRole = (userId, isCommittee) =>
 	catchAsync("user", async (dispatch) => {
+		// Remove and previous alert possible from errors
 		dispatch(resetAlert());
+		// Dispatch loading page
 		dispatch({ type: LOADING_USER });
 		if (isCommittee === "true") {
+			// Make request to promote non-committee user
 			const res = await axios.patch(`/api/users/makeCommittee/${userId}`);
+			// Dispatch updated user detail to user list
 			dispatch({ type: MAKE_COMMITTEE, payload: res.data });
+			// Dispatch alert to user
 			dispatch(
 				setAlert(
 					`${res.data.firstName} ${res.data.lastName} is now a committee member`,
@@ -124,8 +142,11 @@ export const changeRole = (userId, isCommittee) =>
 				)
 			);
 		} else {
+			// Make request to demote a committee member
 			const res = await axios.patch(`/api/users/removeCommittee/${userId}`);
+			// Dispatch updated user detail to user list
 			dispatch({ type: REMOVE_COMMITTEE, payload: res.data });
+			// Dispatch alert to user
 			dispatch(
 				setAlert(
 					`${res.data.firstName} ${res.data.lastName} is back to a normal member`,
