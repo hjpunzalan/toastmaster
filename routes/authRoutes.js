7c98fdae5a6b28ce6f385955b90b43e227f48a09
hjpunzalan <hj.punzalan@hotmail.com>
2020-12-18 18:04:54 +0800

    fix: changed isLoggedIn controller to checkUser

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const authController = require('../controllers/authController');
+const express = require("express");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get('/checkUser', authController.isLoggedIn); // need to be in at the top of app.js client side
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.get("/checkUser", authController.checkUser); // need to be in at the top of app.js client side
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 module.exports = router;
