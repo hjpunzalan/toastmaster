ef256ead3387084586680871080156f660afa2d6
hjpunzalan <hj.punzalan@hotmail.com>
2020-12-30 17:58:01 +0800

    test: change password

--- client/src/actions/__test__/auth.test.js
+++ client/src/actions/__test__/auth.test.js
@@ -8,6 +8,7 @@ import {
 	logoutUser,
 	forgotPassword,
 	resetPassword,
+	changePassword,
 } from "../auth";
 
 describe("AUTH request patterns", () => {
@@ -42,7 +43,7 @@ describe("AUTH request patterns", () => {
 
 		// Test reset alert
 		const msg = "test";
-		const alertType = "success";
+		const alertType = "fail";
 		store.dispatch(setAlert(msg, alertType));
 
 		// Spy mock on history
@@ -57,6 +58,8 @@ describe("AUTH request patterns", () => {
 			})
 		);
 		const { auth, alerts } = store.getState();
+
+		expect(auth.loading).toBe(false);
 
 		// Assert reset alert works
 		expect(alerts.msg.length).toEqual(0);
@@ -83,6 +86,8 @@ describe("AUTH request patterns", () => {
 
 		// Assert login user
 		expect(logoutState.auth.isAuthenticated).toBe(false);
+
+		expect(logoutState.auth.loading).toBe(false);
 	});
 
 	test("should authenticate user if logged in already", async () => {
@@ -105,6 +110,7 @@ describe("AUTH request patterns", () => {
 		const { auth } = store.getState();
 
 		// Assert login user
+		expect(auth.loading).toBe(false);
 		expect(auth.currentUser.email).toBe(user.email);
 	});
 
@@ -144,7 +150,7 @@ describe("AUTH request patterns", () => {
 
 		// Test reset alert
 		const msg = "test";
-		const alertType = "success";
+		const alertType = "fail";
 		store.dispatch(setAlert(msg, alertType));
 
 		// Spy mock on history
@@ -156,6 +162,49 @@ describe("AUTH request patterns", () => {
 			resetPassword({
 				password: user.password,
 				resetToken: "test",
+				history,
+			})
+		);
+		const { auth, alerts } = store.getState();
+
+		// Assert auth loading, alert sent, history push
+		expect(auth.loading).toBe(false);
+		expect(alerts.msg.length).toEqual(1);
+		expect(alerts.alertType).toBe("success");
+		expect(history.push).toHaveBeenCalledWith("/dashboard");
+	});
+
+	test("should change password", async () => {
+		const store = storeFactory();
+
+		moxios.wait(() => {
+			// Define how moxios respond from axios
+			const request = moxios.requests.mostRecent();
+			request.respondWith({
+				status: 200,
+				_id: user._id,
+				email: user.email,
+			});
+		});
+
+		// Test reset alert
+		const msg = "test";
+		const alertType = "fail";
+		store.dispatch(setAlert(msg, alertType));
+
+		// Spy mock on history
+		const history = createBrowserHistory();
+		jest.spyOn(history, "push");
+
+		const formData = {
+			currentPassword: user.password,
+			newPassword: "test1212",
+		};
+
+		// Dispatch login action
+		await store.dispatch(
+			changePassword({
+				formData,
 				history,
 			})
 		);
