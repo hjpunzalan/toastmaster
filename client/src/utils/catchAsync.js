58e546b1e470c7aa116aa1000abea71a61dcfa5e
hjpunzalan <hj.punzalan@hotmail.com>
2021-01-01 19:20:10 +0800

    fix: concurrency issue with catchAsync logout user

--- client/src/utils/catchAsync.js
+++ client/src/utils/catchAsync.js
@@ -12,8 +12,8 @@ import { logoutUser } from "../actions/auth";
 // So when only one argument is provided, it will be fn
 const catchAsync = (type, fn = type) => {
 	return async (dispatch) => {
-		await fn(dispatch).catch((err) => {
-			const errors = err.response.data;
+		await fn(dispatch).catch(async (err) => {
+			const errors = err.response;
 			// The types handles when loading is still true
 			if (errors) dispatch(setAlert(errors.message, "fail"));
 			if (type === "post") dispatch({ type: POST_ERROR });
@@ -25,7 +25,7 @@ const catchAsync = (type, fn = type) => {
 			// Log out user if roles changed --- 403  Forbidden
 			if (err.response.status === 401 || err.response.status === 403) {
 				// Remove the cookie
-				dispatch(logoutUser());
+				await dispatch(logoutUser());
 			}
 			dispatch({
 				type: ERROR,
