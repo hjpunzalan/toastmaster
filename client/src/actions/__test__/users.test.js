11935ccb037bdb5a29a24c1f0f134963be78e4ea
hjpunzalan <hj.punzalan@hotmail.com>
2021-02-02 06:16:33 +0800

    test: update me

--- client/src/actions/__test__/users.test.js
+++ client/src/actions/__test__/users.test.js
@@ -4,7 +4,7 @@ import { createBrowserHistory } from "history";
 import { storeFactory } from "../../utils/testUtils";
 import { setAlert } from "../alerts";
 import { initialState } from "../../reducers/users";
-import { registerUser } from "../users";
+import { registerUser, updateMe } from "../users";
 
 describe("USER request patterns", () => {
 	const testUser = {
@@ -55,5 +55,61 @@ describe("USER request patterns", () => {
 		expect(alerts.msg[0]).toBe(
 			`${testUser.firstName} ${testUser.lastName} was successfully registered and temporary password sent to user's email`
 		);
+	});
+
+	test("should update user", async () => {
+		const store = storeFactory();
+		const mock = new MockAdapter(axios);
+
+		// props
+		const url = "testUrl";
+		const key = "testPhoto";
+		const file = { file: "data" };
+		const updatedUser = {
+			...testUser,
+			photo: `https://toastmaster-user-photo.s3-ap-southeast-2.amazonaws.com/${key}`,
+		};
+
+		// Mock axios request
+		mock
+			.onPost("/api/upload")
+			.reply(200, {
+				url,
+				key,
+			})
+			.onPut(url)
+			.reply(200)
+			.onPatch("/api/users/updateMe")
+			.reply(200, testUser);
+
+		// Test reset alert
+		const msg = "test";
+		const alertType = "fail";
+		store.dispatch(setAlert(msg, alertType));
+
+		// Spy mock on history
+		const history = createBrowserHistory();
+		jest.spyOn(history, "push");
+
+		// Dispatch login action with file
+		await store.dispatch(updateMe(testUser, file, history));
+		const { auth, alerts } = store.getState();
+		// Assert loading
+		expect(auth.loading).toBe(false);
+
+		// Assert current user
+		expect(auth.currentUser).toEqual(updatedUser);
+
+		// Assert modification
+		expect(auth.isModified).toBe(true);
+
+		// Assert history redirection
+		expect(history.push).toHaveBeenCalledWith("/dashboard");
+
+		// Assert reset alert works
+		expect(alerts.msg.length).toEqual(1);
+		// Assert alert sent to user
+		expect(alerts.alertType).toBe("success");
+		expect(alerts.msg[0]).toBe("User updated");
 	});
 });
