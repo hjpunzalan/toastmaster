29c9a55495de8f6b49d397dc2b7072df61ba24ca
hjpunzalan <hj.punzalan@hotmail.com>
2021-03-03 22:54:04 +0800

    test: Register- setup test and test Redirect

---
+++ client/src/components/auth/__test__/Register.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { createBrowserHistory } from "history";
+import { Redirect } from "react-router-dom";
+import { Register, inputs } from "../Register";
+import Spinner from "../../utils/Spinner";
+import { setAlert, resetAlert } from "../../../actions/alerts";
+import { registerUser } from "../../../actions/users";
+
+const history = createBrowserHistory();
+
+const initialProps = {
+	registerUser,
+	users: { loading: false, Moderator: true },
+	auth: { currentUser: { role: "admin" } },
+	history,
+};
+
+// Setup function that returns wrapper
+const setup = (props = {}) => {
+	const setupProps = { ...initialProps, ...props };
+	return shallow(<Register {...setupProps} history={history} />);
+};
+
+// FOR DEBUGGING
+// console.log(wrapper.debug());
+// console.log(wrapper.props());
+
+test("Redirect component when role is user", () => {
+	const wrapper = setup({
+		users: { loading: true, Moderator: false },
+		auth: {
+			currentUser: { role: "user" },
+		},
+	});
+	expect(wrapper.contains(<Redirect to="/dashboard" />)).toBe(true);
+});
