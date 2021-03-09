276955ddb26387b73099e2cfd8c9e40b24085ca0
hjpunzalan <hj.punzalan@hotmail.com>
2021-03-09 20:45:02 +0800

    test:Alert - show alert when theres alert type

--- client/src/components/layout/__test__/Alert.test.js
+++ client/src/components/layout/__test__/Alert.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { Redirect } from "react-router-dom";
 import { Alert } from "../Alert";
 
 const initialProps = {
@@ -20,7 +19,21 @@ const setup = (props = {}) => {
 
 test("No alerts if there's no alert type", () => {
 	const wrapper = setup({
+		alertType: null,
+	});
+	expect(wrapper.find(".Alert").length).toBe(0);
+});
+
+test("Shows alert if there is an alert type of success", () => {
+	const wrapper = setup({
 		alertType: "success",
 	});
 	expect(wrapper.find(".Alert.Alert__success").length).toBe(1);
+});
+
+test("Shows alert if there is an alert type of fail", () => {
+	const wrapper = setup({
+		alertType: "fail",
+	});
+	expect(wrapper.find(".Alert.Alert__fail").length).toBe(1);
 });
