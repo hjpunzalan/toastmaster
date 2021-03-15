14c8d48de461f9b398021a7606ba45110313b429
hjpunzalan <hj.punzalan@hotmail.com>
2021-03-15 22:49:32 +0800

    test: Navbar - render logo and login

--- client/src/components/layout/__test__/Navbar.test.js
+++ client/src/components/layout/__test__/Navbar.test.js
@@ -19,3 +19,15 @@ test("renders nav bar", () => {
 	const wrapper = setup();
 	expect(wrapper.find(".Navbar").length).toBe(1);
 });
+
+test('should  render Logo', () => {
+	const wrapper = setup();
+	expect(wrapper.find(".Navbar__left .Navbar__item .Navbar__logo").length).toBe(1);
+})
+
+
+describe('Not Authenticated', () => {
+	const wrapper = setup({ auth: { isAuthenticated: false } });
+	// Check if theres only login on nav bar link
+	expect(wrapper.find(".Navbar__right .Navbar__item").length).toBe(1)
+})
