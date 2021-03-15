beff3899a8b00a0b8d679a05a2cfdadff8f9ac45
hjpunzalan <hj.punzalan@hotmail.com>
2021-03-15 23:00:54 +0800

    test: Navbar - render text and caption

--- client/src/components/layout/__test__/Navbar.test.js
+++ client/src/components/layout/__test__/Navbar.test.js
@@ -20,10 +20,19 @@ test("renders nav bar", () => {
 	expect(wrapper.find(".Navbar").length).toBe(1);
 });
 
-test('should  render Logo', () => {
+test('render Logo', () => {
 	const wrapper = setup();
 	expect(wrapper.find(".Navbar__left .Navbar__item .Navbar__logo").length).toBe(1);
 })
+
+test('Render text and caption', () => {
+	const wrapper = setup();
+	const header = wrapper.find(".Navbar__left .Navbar__header")
+	expect(header.props().children.length).toBe(2)
+	expect(header.find(".Navbar__item.Navbar__header-text").props().children.length).not.toBe(0);
+	expect(header.find(".Navbar__item.Navbar__header-caption").props().children.length).not.toBe(0);
+})
+
 
 
 describe('Not Authenticated', () => {
