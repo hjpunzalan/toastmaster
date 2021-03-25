df2a74ce50507f66569ac25ec77ffbc127d6237b
hjpunzalan <hj.punzalan@hotmail.com>
2021-03-25 18:15:45 +0800

    test: Dashboard - get all announcements

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,6 +1,13 @@
 // Create react app already set this file to run before each test
 import Enzyme from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import enableHooks from 'jest-react-hooks-shallow';
+
+// jest-react-hooks-shallow
+// pass an instance of jest to `enableHooks()`
+// This package makes React Hooks (namely, useEffect() and useLayoutEffect()) work with shallow rendering. 
+// In other words, you can use enzyme.
+enableHooks(jest);
 
 Enzyme.configure({
 	adapter: new EnzymeAdapter(),
