4940841f57a6e4a632d776efd0f743ce395c8813
hjpunzalan <hj.punzalan@hotmail.com>
2021-03-27 21:38:15 +0800

    test: Dashboard - render spinner,announcements, user

--- client/src/components/pages/Dashboard/__test__/Dashboard.test.js
+++ client/src/components/pages/Dashboard/__test__/Dashboard.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { Announcements } from "../Announcements";
+import Announcements from '../Announcements';
 import { Dashboard } from "../Dashboard";
-import { User } from "../User";
+import  User  from "../User";
+import Spinner from '../../../utils/Spinner';
 import {
 	getAnnouncements,
 } from "../../../../actions/announcements";
@@ -27,9 +28,32 @@ const setup = (props = {}) => {
 
 test('should get all announcements', () => {
 	const getAnnouncementsMock = jest.fn()
-	const wrapper = setup({ getAnnouncements: getAnnouncementsMock });
-	
+	setup({ getAnnouncements: getAnnouncementsMock });
 	expect(getAnnouncementsMock).toHaveBeenCalled()
-
 })
+
+test('if loading is true, render spinner', () => {
+	const wrapper = setup({ announcements: { loading: true } })
+	expect(wrapper.contains(<Spinner />)).toBe(true)
+})
+
+test('render announcements if loading is false', () => {
+	const wrapper = setup()
+	expect(wrapper.find(".Dashboard").containsMatchingElement(<Announcements />)).toBe(true)
+})
+
+test('renders user if announcements is not on edit mode', () => {
+	const wrapper = setup({
+		auth: {
+			currentUser: { _id: "test" },
+			isModified: false
+	},
+		announcements: { edit: false }
+	})
+	expect(wrapper.find(".Dashboard").containsMatchingElement(<User />)).toBe(true)
+	expect(wrapper.find("User").props()).toEqual({ currentUser: { _id: 'test' }, isModified: false })
+})
+
+
+
 
