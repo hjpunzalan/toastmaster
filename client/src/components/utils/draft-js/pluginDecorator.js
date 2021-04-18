06341c0299a1d0ac6db1d5f35e33f5d0b1d5899a
hjpunzalan <hj.punzalan@hotmail.com>
2021-04-18 14:42:57 +0800

    fix: plugin decorator

--- client/src/components/utils/draft-js/pluginDecorator.js
+++ client/src/components/utils/draft-js/pluginDecorator.js
@@ -18,9 +18,8 @@ const getPluginDecoratorArray = (listOfPlugins) => {
 const pluginDecorator = (listOfPlugins) => {
 	// I can't quite remember why I had to wrap things in this exact way, but found that I ran into
 	// errors if I did not both have a MultiDecorator and a CompositeDecorator wrapping
-	return new MultiDecorator([
-		new CompositeDecorator(getPluginDecoratorArray(listOfPlugins)),
-	]);
+	return new CompositeDecorator(getPluginDecoratorArray(listOfPlugins))
+	
 };
 
 export default pluginDecorator;
