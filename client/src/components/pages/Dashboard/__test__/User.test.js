ef4440bdd8007675848f49a20cb218e24608278a
hjpunzalan <hj.punzalan@hotmail.com>
2021-05-05 22:22:50 +0800

    test: User.js render user photo

--- client/src/components/pages/Dashboard/__test__/User.test.js
+++ client/src/components/pages/Dashboard/__test__/User.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { User } from "../User";
 import { createBrowserHistory } from "history";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 
 const history = createBrowserHistory();
@@ -23,4 +24,21 @@ const setup = (props = {}) => {
 test('render dashboard right/ User component', () => {
     const wrapper = setup();
     expect(wrapper.find(".Dashboard__right").length).toBe(1)
+})
+
+describe('render user photo', () => {
+    test('render default photo', () => {
+        const wrapper = setup();
+        expect(wrapper.find(".Dashboard__user-photo").length).toBe(1)
+        expect(wrapper.find(".Dashboard__user-photo").props().src).toBe("anonymous.png")
+    })
+    test("render uploaded photo", () => {
+        const wrapper = setup({
+            currentUser: {
+               photo: "user.png"
+           }});
+        expect(wrapper.find(".Dashboard__user-photo").length).toBe(1)
+        expect(wrapper.find(".Dashboard__user-photo").props().src).toBe("user.png")
+    })
+    
 })
