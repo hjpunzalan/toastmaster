01d704ec989f899be5c4ef9710cf2cf9d300efc1
hjpunzalan <hj.punzalan@hotmail.com>
2021-10-16 01:05:23 +0800

    fix: Alert test props

--- client/src/components/layout/__test__/Alert.test.js
+++ client/src/components/layout/__test__/Alert.test.js
@@ -26,14 +26,22 @@ test("No alerts if there's no alert type", () => {
 
 test("Shows alert if there is an alert type of success", () => {
 	const wrapper = setup({
+		msg: ["test"],
 		alertType: "success",
+		error: {}
 	});
 	expect(wrapper.find(".Alert.Alert__success").length).toBe(1);
 });
 
 test("Shows alert if there is an alert type of fail", () => {
+		const error = {
+		status: 401,
+		msg: "fail message",
+	};
 	const wrapper = setup({
+		msg: ["test"], 
 		alertType: "fail",
+		error
 	});
 	expect(wrapper.find(".Alert.Alert__fail").length).toBe(1);
 });
@@ -42,15 +50,22 @@ test("should show alert message when alert type is success", () => {
 	const wrapper = setup({
 		msg: ["test"],
 		alertType: "success",
+		error: {}
 	});
 	expect(wrapper.find("strong").props().children).toBe("Success: ");
 	expect(wrapper.find("p").props().children.length).not.toBe(0);
 });
 
 test("should show alert message when alert type is fail", () => {
+	const error = {
+		status: 401,
+		msg: "fail message",
+	};
+
 	const wrapper = setup({
-		msg: ["test"],
+		msg: ["test"], 
 		alertType: "fail",
+		error
 	});
 	expect(wrapper.find("strong").props().children).toBe("Warning: ");
 	expect(wrapper.find("p").props().children.length).not.toBe(0);
