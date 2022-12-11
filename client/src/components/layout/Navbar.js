27a932fb2a727c01ddabafcec09769ee90633e88
hjpunzalan <hj.punzalan@hotmail.com>
2022-12-12 01:55:18 +0800

    remove text decoration on navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,7 +12,7 @@ export const Navbar = ({ auth: { isAuthenticated }, logoutUser }) => {
 	return (
 		<>
 			<ul className={`Navbar ${!isAuthenticated ? "Navbar-withLogin" : ""}`}>
-				<Link to="/">
+				<Link to="/" style={{ textDecoration: "none" }}>
 					<div className="Navbar__left">
 						<li className="Navbar__item">
 							<img className="Navbar__logo" src={logo} alt="logo" />
